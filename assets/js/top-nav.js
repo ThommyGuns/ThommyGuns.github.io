d676add182ad4f95bf612d1920bb590677a6c917
tommybartlettiii <tommyb83@hotmail.com>
2015-06-04 20:42:52 -0400

    Revert "reverty"
    This reverts commit b90208a6877105e33891529ab7891156dd7fab28.

--- assets/js/top-nav.js
+++ assets/js/top-nav.js
@@ -31,10 +31,17 @@
 document.write('\
  <nav class="navbar navbar-default top-nav">\
    <div class="container">\
+	     <div class="navbar-header">\
+			<a href="Home.html" class="navbar-brand"><img class="pull-left brand-image" src="assets/EF.jpg"></a>\
+			<h3 class="main-title pull-right">Eades Fitness 24/7</h3>\
+		 </div>\
 		 <div>\
 			<ul class="nav navbar-nav pull-right">\
 				<li id="home"><a href="Home.html">Home</a></li>\
-				<li id="about"><a href="#">About</a></li>\
+				<li id="memberships"><a href="#">Memberships</a></li>\
+				<li id="nutrition"><a href="#">Nutrition</a></li>\
+				<li id="trainers"><a href="personal_trainers.html">Trainers</a></li>\
+				<li id="workouts"><a href="#">Workouts</a></li>\
 				<li class="dropdown"><a class="dropdown-toggle"\
 					data-toggle="dropdown" href="#">Events<span class="caret"></span></a>\
 					<ul class="dropdown-menu">\
